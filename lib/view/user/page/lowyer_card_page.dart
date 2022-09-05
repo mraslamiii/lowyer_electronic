fe6a94662de7a01cf7f4e9600154650a0d0553c4
Amin Alizadeh <aab.alizadeh@gmail.com>
2022-09-05 17:37:34 +0430

    debug

--- lib/view/user/page/lowyer_card_page.dart
+++ lib/view/user/page/lowyer_card_page.dart
@@ -5,10 +5,9 @@ import 'package:kanoon_dadgostari/view/user/controller/lawyer_controller.dart';
 import 'package:kanoon_dadgostari/view/widgets/customScaffold/customScaffold.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:get/get.dart';
 
-import '../../auth/login_page/page/tst_page.dart';
+import '../../widgets/back_widget/back_widget.dart';
 
 class LawyerCardPage extends StatelessWidget {
   const LawyerCardPage({Key? key}) : super(key: key);
@@ -16,9 +15,7 @@ class LawyerCardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<LawyerController>(
-
       init: LawyerController(),
-
       builder: (controller) => customScaffold(
           bottomAppBar: BottomAppBar(
               elevation: 0,
@@ -27,14 +24,24 @@ class LawyerCardPage extends StatelessWidget {
                 child: Row(
                   children: [
                     Expanded(
-                        flex: 10,
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text("درخواست جدید"))),
-                    Expanded(flex: 1, child: SizedBox()),
+                        flex: 1,
+                        child: SizedBox(
+                          width: fullWidth,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("درخواست جدید")),
+                        )),
+                    SizedBox(
+                      width: standardSize,
+                    ),
                     Expanded(
-                        flex: 10,
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text("درخواست جدید"))),
+                        flex: 1,
+                        child: SizedBox(
+                          width: fullWidth,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("درخواست جدید")),
+                        )),
                   ],
                 ),
               )),
@@ -43,35 +50,37 @@ class LawyerCardPage extends StatelessWidget {
             title: const Text(
               "وکیل کارت",
             ),
+            automaticallyImplyLeading: false,
+            leading: backIcon(),
           ),
           body: Padding(
             padding: EdgeInsets.symmetric(
-                vertical: largeSize, horizontal: largeSize),
+                vertical: standardSize, horizontal: standardSize),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-
               children: [
-                GestureDetector(onTap: () => Get.off(TestPage()) ,child: const BankCard()),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: standardSize),
-                  child: Text("تاریخچه", style: Get.theme.textTheme.headline5,textAlign: TextAlign.start),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    controller: controller.controller,
-                  itemCount: 10,
-                  itemBuilder: (context, index) => historyCard(),
-                  ),
-                )
+                const BankCard(),
+                // Padding(
+                //   padding: EdgeInsets.symmetric(vertical: standardSize),
+                //   child: Text("تاریخچه",
+                //       style: Get.theme.textTheme.headline5,
+                //       textAlign: TextAlign.start),
+                // ),
+                // Expanded(
+                //   child: ListView.builder(
+                //     shrinkWrap: true,
+                //     controller: controller.controller,
+                //     itemCount: 10,
+                //     itemBuilder: (context, index) => historyCard(),
+                //   ),
+                // )
               ],
             ),
           )),
     );
   }
-
-
 }
+
 Widget historyCard() {
   return Container(
     margin: EdgeInsets.symmetric(vertical: mediumSize),
@@ -83,10 +92,9 @@ Widget historyCard() {
       children: [
         historyWidgetItem(
             'assets/icons/Paper.svg', "نوع درخواست", "صدور کارت جدید"),
-        Divider(),
-        historyWidgetItem(
-            'assets/icons/stickynote.svg', " تاریخ", "28/8/1401"),
-        Divider(),
+        const Divider(),
+        historyWidgetItem('assets/icons/stickynote.svg', " تاریخ", "28/8/1401"),
+        const Divider(),
         historyWidgetItem(
             'assets/icons/clipboard-tick.svg', "وضعیت", "در حال بررسی"),
       ],
@@ -102,82 +110,95 @@ Widget historyWidgetItem(String icon, String title, String subTitle) {
         SvgPicture.asset(icon),
         Padding(
           padding: EdgeInsets.only(right: smallSize),
-          child: Text("${title} :",
+          child: Text("$title :",
               style: Get.theme.textTheme.subtitle1
                   ?.copyWith(color: AppColors.primaryColor)),
         ),
-        Expanded(child: SizedBox()),
+        const Expanded(child: SizedBox()),
         Text(subTitle, style: Get.theme.textTheme.subtitle1),
       ],
     ),
   );
 }
+
 class BankCard extends GetView<LawyerController> {
   const BankCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () =>  AnimatedContainer(
+      () => AnimatedContainer(
         clipBehavior: Clip.antiAlias,
         alignment: Alignment.center,
-        duration: Duration(milliseconds: 1000),
+        duration: const Duration(milliseconds: 1000),
         width: fullWidth,
         decoration: BoxDecoration(
             color: AppColors.primaryColor,
-            borderRadius: BorderRadius.all(Radius.circular(standardRadius))),
+            boxShadow: [
+              BoxShadow(
+                  color: AppColors.primaryColor.withOpacity(0.5),
+                  spreadRadius: 2,
+                  offset: const Offset(0, 4),
+                  blurRadius: 10)
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(smallRadius))),
         height: controller.heightCard.value,
         child: Stack(children: [
           Positioned(
               top: -25,
               right: -15,
               child: SvgPicture.asset('assets/icons/dots_btm.svg')),
+          Positioned(
+              bottom: -25,
+              left: -15,
+              child: SvgPicture.asset('assets/icons/dots_btm.svg')),
           Align(
               alignment: Alignment.center,
               child: Text(
                 "6274 1212 0298 5289",
+                textDirection: TextDirection.ltr,
                 style: GoogleFonts.poppins(
                     color: Colors.white,
-                    fontSize: fullWidth / 14,
+                    fontSize: fullWidth / 17,
                     fontWeight: FontWeight.w500),
               )),
           Positioned(
-              top: largeSize,
-              left: largeSize,
-              right: largeSize,
+              top: standardSize,
+              left: standardSize,
+              right: standardSize,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset(
                     'assets/icons/ic_bank.png',
-                    scale: 2,
+                    scale: 2.5,
                   ),
                   const Expanded(child: SizedBox()),
                   Text(
                     "وضعیت : فعال",
-                    style: Get.theme.textTheme.subtitle1!
+                    style: Get.theme.textTheme.subtitle2!
                         .copyWith(color: Colors.white),
                   )
                 ],
               )),
           Positioned(
-              bottom: largeSize,
-              left: largeSize,
-              right: largeSize,
+              bottom: standardSize,
+              left: standardSize,
+              right: standardSize,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     "تاریخ انقضا : 02/04",
-                    style: Get.theme.textTheme.subtitle1!
+                    style: Get.theme.textTheme.subtitle2!
                         .copyWith(color: Colors.white),
                   ),
                   const Expanded(child: SizedBox()),
                   Text(
                     "Cvv2 : 874",
-                    style: Get.theme.textTheme.subtitle1!
+                    style: Get.theme.textTheme.subtitle2!
                         .copyWith(color: Colors.white),
                   )
                 ],
