2d0fb064ea9a3ed14de914955a0e41ff41606f60
abbas <abas.aslmi0007@gmail.com>
2022-09-05 17:51:12 +0430

    ui

--- lib/res/dimens/font_size.dart
+++ lib/res/dimens/font_size.dart
@@ -6,6 +6,8 @@ double subTitleSize = Get.width / 25;
 
 double headline5TextSize = Get.width/15.6; //todo 24
 
+double subtitle1TextSize = Get.width/20; //todo 19
+
 double subtitle2TextSize = Get.width/23.4; //todo 16
 
 double body1TextSize = Get.width/29.2; //todo 14.0
