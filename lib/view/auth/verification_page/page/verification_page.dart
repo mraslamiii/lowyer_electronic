1681dcb00a95351c9e2f0963807af6ec3f35c613
Amin Alizadeh <aab.alizadeh@gmail.com>
2022-10-11 16:35:49 +0330

    debug

--- lib/view/auth/verification_page/page/verification_page.dart
+++ lib/view/auth/verification_page/page/verification_page.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -7,6 +8,7 @@ import 'package:kanoon_dadgostari/view/auth/verification_page/controller/verify_
 import 'package:kanoon_dadgostari/view/widgets/customScaffold/customScaffold.dart';
 import 'package:kanoon_dadgostari/view/widgets/progress_button/progress_button.dart';
 
+import '../../../../app/app_pages.dart';
 import '../../login_page/page/login_page.dart';
 import '../widgets/pin_code_fields.dart';
 
@@ -21,71 +23,99 @@ class VerificationPage extends StatelessWidget {
       initState: (state) {
         phone = Get.arguments as String;
       },
-      builder: (controller) => customScaffold(
-          context: context,
-          appBar: AppBar(
-            title: const Text(
-              "ورود به اپلیکیشن",
-            ),
-          ),
-          body: SizedBox(
-            width: Get.width,
-            height: Get.height,
-            child: Padding(
-              padding: EdgeInsets.all(largeSize),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("کد پیامک شده را وارد کنید",
-                      style: Get.theme.textTheme.subtitle1),
-                  Padding(
-                    padding: EdgeInsets.only(top: standardSize),
-                    child: Directionality(
-                      textDirection: TextDirection.ltr,
-                      child: PinCodeField(),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Get.offAll(LoginPage());
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.only(top: smallSize),
-                      child: Row(
-                        children: [
-                          SvgPicture.asset('assets/icons/ic_edit_underline.svg',
-                              color: AppColors.primaryColor),
-                          Padding(
-                              padding: EdgeInsets.only(right: xSmallSize),
-                              child: Text(
-                                "ویرایش شماره تلفن",
-                                style: Get.theme.textTheme.subtitle2
-                                    ?.copyWith(color: AppColors.primaryColor),
-                              ))
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Expanded(child: SizedBox()),
-                  SizedBox(
-                    width: fullWidth,
-                    child: Obx(
-                      () => progressButton(
-                          isProgress: controller.isBusyLogin.value,
-                          onTap: controller.isValid.value &&
-                                  controller.isBusyLogin.isFalse
-                              ? () {
-                                  controller.fetchData(phone);
-                                }
-                              : null,
-                          text: "ارسال کد تایید"),
-                    ),
-                  )
-                ],
+      builder: (controller) => WillPopScope(
+        onWillPop: _backToLogin,
+        child: customScaffold(
+            context: context,
+            appBar: AppBar(
+              title: const Text(
+                "ورود به اپلیکیشن",
               ),
             ),
-          )),
+            body: SizedBox(
+              width: Get.width,
+              height: Get.height,
+              child: Padding(
+                padding: EdgeInsets.all(largeSize),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("کد پیامک شده را وارد کنید",
+                        style: Get.theme.textTheme.subtitle1),
+                    Padding(
+                      padding: EdgeInsets.only(top: standardSize),
+                      child: Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: PinCodeField(),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _backToLogin();
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(top: smallSize),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset('assets/icons/ic_edit_underline.svg',
+                                color: AppColors.primaryColor),
+                            Padding(
+                                padding: EdgeInsets.only(right: xSmallSize),
+                                child: Text(
+                                  "ویرایش شماره تلفن",
+                                  style: Get.theme.textTheme.subtitle2
+                                      ?.copyWith(color: AppColors.primaryColor),
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Expanded(child: SizedBox()),
+                    SizedBox(
+                      width: fullWidth,
+                      child: Obx(
+                        () => progressButton(
+                            isProgress: controller.isBusyLogin.value,
+                            onTap: controller.isValid.value &&
+                                    controller.isBusyLogin.isFalse
+                                ? () {
+                                    controller.fetchData(phone);
+                                  }
+                                : null,
+                            text: "ارسال کد تایید"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )),
+      ),
     );
   }
+}
+
+Future<bool> _backToLogin() async {
+  var result = await showOkCancelAlertDialog(
+      context: Get.context!,
+      cancelLabel: "ادامه",
+      isDestructiveAction: true,
+      builder: (context, child) => Theme(
+        data: ThemeData(
+          textTheme: Get.textTheme,
+        ),
+        child: Directionality(
+            textDirection: TextDirection.rtl, child: child),
+      ),
+      defaultType: OkCancelAlertDefaultType.cancel,
+      okLabel: "خروج",
+      message: "برای ویرایش شماره تلفن اطمینان دارید؟",
+      title: "انصراف از ورود");
+
+  if(result ==OkCancelResult.ok){
+    Get.offAllNamed(Routes.loginPage);
+  }
+
+  print(result);
+  return false;
 }
